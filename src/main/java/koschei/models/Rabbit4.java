d80734d7a5d74ff2997f02f40ba57ba8431bbafe
Maksim <yaroshevichmaks@gmail.com>
2024-04-04 14:44:13 +0300

    add project

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
+    private Duck5 duck5;
+
+    @Autowired
+    public Rabbit4(Duck5 duck) {
+        this.duck5 = duck;
+    }
+
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck5.toString();
     }
 }
